7b71b9aa53ff10403079bdcd834bac122a969530
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-02-10 21:58:46 +0530

    Initial Commit

--- app/src/main/java/com/sumayya/a7minutesworkoutapp/MainActivity.kt
+++ app/src/main/java/com/sumayya/a7minutesworkoutapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.sumayya.a7minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.sumayya.a7minutesworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Here we will start the exercise.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
